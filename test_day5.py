031fdfdbcf9dcafe16a99b4cf8d06d95a2ed543d
James Peach <james_peach01@hotmail.co.uk>
2023-12-11 08:14:11 +0000

    day5 part2 finally...

--- test_day5.py
+++ test_day5.py
@@ -35,69 +35,33 @@ def parse(lines):
     return data
 
 
+def map_range(curent_range, map):
+    range_start, range_length = curent_range
+
+    while range_length > 0:
+        found = False
+        for destination_range_start, source_range_start, length in map:
+            if source_range_start <= range_start and source_range_start + length > range_start:
+                mapped_range_offset = range_start - source_range_start
+                mapped_range_length = min(length - mapped_range_offset, range_length)
+                range_offset = 0 - source_range_start + destination_range_start 
+
+                yield (range_start + range_offset, mapped_range_length)
+
+                range_length -= mapped_range_length
+                range_start += mapped_range_length
+                found = True
+                break
+        if not found:
+            yield (range_start, range_length)
+            range_length = 0
+
+
+
 def map_ranges(curent_ranges, map):
+    new_ranges = [range for curent_range in curent_ranges for range in map_range(curent_range, map)]
 
-    items = (
-        [(range_start, "1.start_source") for range_start, range_length in curent_ranges]
-        + [
-            (range_start + range_length -1, "2.end_source")
-            for range_start, range_length in curent_ranges
-        ]
-        + [
-            (
-                source_range_start,
-                "0.map_start",
-                0 - source_range_start + destination_range_start,
-            )
-            for destination_range_start, source_range_start, length in map
-        ]
-        + [
-            (
-                source_range_start + length,
-                "0.map_end",
-            )
-            for destination_range_start, source_range_start, length in map
-        ]
-    )
-
-    items.sort(key=lambda x: (x[0], x[1]))
-
-    def maybe_new_range(start_source, map_offset, end, new_ranges):
-        if start_source is not None and map_offset is not None:
-            new_ranges.append((start_source + map_offset, 1 + end - start_source))
-
-    def maybe_add_noop_range(start_source, map_offset, end, new_ranges):
-        if start_source is not None and map_offset is None:
-            new_ranges.append((start_source, 1+ end - start_source))
-
-    new_ranges = []
-    map_offset = None
-    start_source = None
-    for item in items:
-        if item[1] == "1.start_source":
-            start_source = item[0]
-        elif item[1] == "0.map_start":
-            maybe_add_noop_range(start_source, map_offset, item[0], new_ranges)
-            map_offset = item[2]
-        elif item[1] == "2.end_source":
-            maybe_new_range(start_source, map_offset, item[0], new_ranges)
-            maybe_add_noop_range(start_source, map_offset, item[0], new_ranges)
-            start_source = None
-        elif item[1] == "0.map_end":
-            maybe_new_range(start_source, map_offset, item[0], new_ranges)
-            map_offset = None
-            start_source = item[0] if start_source is not None else None
-        else:
-            raise Exception("unknown item type")
-    
     return new_ranges
-
-"""
-49 53 8
-0 11 42
-42 0 7
-57 7 4
-"""
 
 def test_map_ranges():
     map = [(49, 53, 8), (0, 11, 42), (42, 0, 7), (57, 7, 4)]
@@ -179,5 +143,6 @@ humidity-to-location map:
     assert (35,46) == solve(data)
 
     data = parse(list(puzzle_input.lines(5)))
-    assert (388071289, 219974726) == solve(data)
+    assert (388071289, 84206669) == solve(data)
+    assert 219974726 > solve(data)[1], "part 2 is too high"
 
